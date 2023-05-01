109fc79ac1cf0f1147382f0499cd7060979c22af
bookfeather <sherry920517@gmail.com>
2023-05-01 15:26:49 +0800

    hw10

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,5 @@
 import 'bootstrap/dist/css/bootstrap.css';
 import 'antd/dist/reset.css';
-<<<<<<< HEAD
-=======
-
->>>>>>> da84d6f5ecc2a81c72d7a0c9a74a82b8db44d730
 import Home from './pages/Home'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 const queryClient = new QueryClient();
